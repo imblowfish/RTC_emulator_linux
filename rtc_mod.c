f9d691d4ca0e73f2c5bf177181e0525728d175ae
Yury Getman <stagg7777@gmail.com>
2020-05-27 00:31:00 +0300

    proc read/write

--- rtc_mod.c
+++ rtc_mod.c
@@ -2,27 +2,31 @@
 #include <linux/fs.h>
 #include <linux/module.h>
 #include <linux/uaccess.h>
-
+#include <linux/proc_fs.h>
 #include <linux/device.h>
 #include <linux/version.h>
 #include <linux/init.h>
 #include <linux/cdev.h>
 //#include "mod.h"
+
+
 // Добавление устройства в /dev/rtcN
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Yury Getman <stagg7777@gmail.com>");
 
 // инициализация модуля
-static int __init dev_init(void);
+static int __init rtc_init(void);
 // удаление модуля
-static void __exit dev_exit(void);
+static void __exit rtc_exit(void);
 // чтение данных
-static ssize_t rtc_mod_read(struct file *f, char *buf, size_t cnt, loff_t *ppos);
+static ssize_t rtc_read(struct file *f, char *buf, size_t cnt, loff_t *ppos);
+// запись
+static ssize_t rtc_write(struct file *f, const char *buf, size_t cnt, loff_t *pos);
 // создание устройства
-static int rtc_mod_open(struct inode *n, struct file *f);
+static int rtc_open(struct inode *n, struct file *f);
 // удаление устройства
-static int rtc_mod_release(struct inode *n, struct file *f);
+static int rtc_release(struct inode *n, struct file *f);
 
 // диапазон номеров устройств(миноров)
 #define DEV_FIRST 0
@@ -30,6 +34,8 @@ static int rtc_mod_release(struct inode *n, struct file *f);
 #define DEV_NAME "rtc_"
 // имя модуля
 #define MOD_NAME "rtc_module"
+#define LEN_MSG 150
+#define NAME_NODE "rtc_0"
 
 static char* hello = "Hello\n";
 static int major=0;
@@ -41,43 +47,75 @@ static struct class *devclass;
 // читаем параметр major
 module_param(major, int, S_IRUGO); // S_IRUGO - может читаться, кем угодно, но не может быть изменен
 
-static int rtc_mod_open(struct inode *n, struct file *f){
+static char *get_buf(void){
+	static char buf_msg[LEN_MSG+1] = "..1..2..3..4..5\n";
+	return buf_msg;
+}
+
+// ЭТО ВСЕ В dev
+
+static int rtc_open(struct inode *n, struct file *f){
 	if(deviceOpen)
 		return -EBUSY;
 	deviceOpen = !deviceOpen;
 	return 0;
 }
 
-static int rtc_mod_release(struct inode *n, struct file *f){
+static int rtc_release(struct inode *n, struct file *f){
 	deviceOpen = !deviceOpen;
 	return 0;
 }
 
-static ssize_t rtc_mod_read(struct file *f, char *buf, size_t cnt, loff_t *ppos){
-	int len = strlen(hello);
-	printk(KERN_INFO "read: %ld\n", cnt);
-	if(cnt < len)
-		return -EINVAL;
-	if(*ppos){
-		printk(KERN_INFO "read return: 0\n");
+static ssize_t rtc_read(struct file *f, char *buf, size_t cnt, loff_t *ppos){
+	char *buf_msg = get_buf();
+	int res;
+	printk(KERN_INFO "read: %ld bytes {ppos%}\n", (long)cnt, *ppos);
+	if(*ppos >= strlen(buf_msg)){
+		*ppos = 0;
+		printk(KERN_INFO "EOF\n");
 		return 0;
 	}
-	if(copy_to_user(buf, hello, len))
-		return -EINVAL;
-	*ppos = len;
-	printk(KERN_INFO "read return: %d\n", len);
+	if(cnt > strlen(buf_msg) - *ppos){
+		cnt = strlen(buf_msg) - *ppos;
+	}
+	res = copy_to_user((void*)buf, buf_msg + *ppos, cnt);
+	*ppos += cnt;
+	printk(KERN_INFO "return %ld bytes\n", (long)cnt);
+	return cnt;
+}
+
+static ssize_t rtc_write(struct file *f, const char *buf, size_t cnt, loff_t *ppos){
+	//proc
+	char *buf_msg = get_buf();
+	int res;
+	uint len = cnt < LEN_MSG? cnt: LEN_MSG;
+	printk("write: %ld bytes\n", (long)cnt);
+	res = copy_from_user(buf_msg, (void*)buf, len);
+	buf_msg[len] = '\0';
+	printk(KERN_INFO "put %d bytes\n", len);
 	return len;
 }
 
 static const struct file_operations dev_fops = {
 	.owner = THIS_MODULE,
-	.open = rtc_mod_open,
-	.release = rtc_mod_release,
-	.read = rtc_mod_read
+	.open = rtc_open,
+	.release = rtc_release,
+	.read = rtc_read,
+	.write = rtc_write
 };
 
-static int __init dev_init(void){
+static int __init rtc_init(void){
 	int res;
+	//!!!
+	struct proc_dir_entry *own_proc_node;
+	own_proc_node = proc_create(NAME_NODE, S_IFREG|S_IRUGO|S_IWUGO, NULL, &dev_fops);
+	if(!own_proc_node){
+		printk(KERN_ERR "can't create /proc/%s\n", NAME_NODE);
+		return -ENOENT;
+	}
+	printk(KERN_INFO "/proc/%s installed\n", NAME_NODE);
+	//!!!
+	
 	// dev_t тип для представления номера устройства в ядре
 	dev_t dev;
 	// если задали в параметрах major
@@ -113,7 +151,10 @@ static int __init dev_init(void){
 	return 0;
 }
 
-static void __exit dev_exit(void){
+static void __exit rtc_exit(void){
+	//!!!
+	remove_proc_entry(NAME_NODE, NULL);
+	//!!!
 	dev_t dev;
 	dev = MKDEV(major, DEV_FIRST);
 	device_destroy(devclass, dev);
@@ -123,8 +164,10 @@ static void __exit dev_exit(void){
 	printk(KERN_INFO "rtc_module removed\n");
 } 
 
-module_init(dev_init);
-module_exit(dev_exit);
+module_init(rtc_init);
+module_exit(rtc_exit);
+
+//
 
 
 
